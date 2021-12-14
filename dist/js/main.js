0213c400d7ed40cc039cd638964a18eea61981db
ASIFIWE Emmanuel <emmanuel2koestner@gmail.com>
2021-12-14 11:56:08 -0800

    initializing great brand for emmanuel

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,8 +3,12 @@ const navContainer = document.querySelector('.header__nav--container');
 const cardList = document.querySelectorAll('#dashboard-card');
 const infoReadMoreBtn = document.getElementById('info-readmore');
 const modalContainer = document.querySelector('.about--modal');
-
-console.log(cardList);
+const experienceBtn = document.querySelector('#exp-btn');
+const experienceModal = document.querySelector('.experience__modal-container');
+const closeBtn = document.getElementById('btn-deactive--modal');
+const closeBtnModal = document.getElementById('btn--modal');
+const experienceBtns = document.querySelectorAll('.experience-sec__list--item');
+const experienceContainers = document.querySelectorAll('.experience-container');
 
 
 const addActive = (container, img= '') => {
@@ -17,7 +21,6 @@ const removeActive = (container, img= '') => {
 }
 
 toggleBtn.addEventListener('click', () => {
-    // navContainer.classList.toggle('active');
     const img = toggleBtn.querySelector('img');
     if (navContainer.classList.contains('active')) {
         removeActive(navContainer, img);
@@ -73,11 +76,58 @@ cardList.forEach(card => {
 //     }
 // })
 
+
 infoReadMoreBtn.addEventListener('click', () => {
     modalContainer.classList.add('active');
+})
 
-})
-const closeBtn = document.getElementById('btn-deactive--modal')
 closeBtn.addEventListener('click', () => {
-    modalContainer.classList.remove('active');
+    if (modalContainer.classList.contains('active')) {
+        modalContainer.classList.remove('active');
+    }
 })
+
+closeBtnModal.addEventListener('click', () => {
+    if (experienceModal.classList.contains('active')) {
+        experienceModal.classList.remove('active');
+    }
+})
+
+experienceBtn.addEventListener('click', () => {
+    experienceModal.classList.add('active');
+})
+
+
+const experienceActive = (experienceSelector) => {
+    experienceContainers.forEach(experience => {
+        if (experienceSelector === 'rba') {
+            if (experience.id === 'rba-experience') {
+                experience.classList.add('active');
+            }
+        } else if (experienceSelector === 'school') {
+            if (experience.id === 'school-experience') {
+                experience.classList.add('active');
+            }
+        } else if (experienceSelector == 'andela') {
+            if (experience.id === 'andela-experience') {
+                experience.classList.add('active');
+            }
+        }
+    })
+}
+
+experienceBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (btn.id === 'experience-1') {
+            btn.classList.add('active');
+            experienceActive('rba');
+        } else if (btn.id === 'experience-2') {
+            btn.classList.add('active');
+            experienceActive('school');
+        } else {
+            btn.classList.add('active');
+            experienceActive('andela');
+        }
+    })
+})
+
